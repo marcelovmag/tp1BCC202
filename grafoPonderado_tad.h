11cfa3976acf49cf6aff238beb61e3dffcf58c44
marcelovmag <81254324+marcelovmag@users.noreply.github.com>
2023-10-10 22:47:40 -0300

    primeiro commit
    - Criação dos arquivos iniciais

---
+++ grafoPonderado_tad.h
@@ -0,0 +1,18 @@
+#ifndef grafoPonderado_tad_h
+#define grafoPonderado_tad_h
+
+#include <stdio.h>
+
+typedef struct grafoPon GrafoPon;
+
+void alocarGrafo();
+
+void desalocarGrafo();
+
+void leGrafo();
+
+void encontraCaminho();
+
+void imprimeCaminho();
+
+#endif /* grafoPonderado_tad_h */
